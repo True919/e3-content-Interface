07263690f9cf64143aee4a3d98d7d4128fec617e
wx <15621555523@163.com>
2017-12-12 00:44:07 +0800

    wx

---
+++ src/main/java/cn/e3mall/service/ContentService.java
@@ -0,0 +1,29 @@
+package cn.e3mall.service;
+
+import java.util.List;
+
+import cn.e3mall.pojo.TbContent;
+import cn.e3mall.utils.AdItem;
+import cn.e3mall.utils.DatagridPagebean;
+import cn.e3mall.utils.E3mallResult;
+
+public interface ContentService {
+	/**
+	 * 需求:根据外键查询广告内容数据进行分页展示
+	 * 参数:Long categoryId,Integer page,Integer rows
+	 * 返回值:DatagridPagebean
+	 */
+	public DatagridPagebean findContentListByPage(Long categoryId,Integer page,Integer rows);
+	/**
+	 * 需求:保存广告内容数据
+	 * 参数:TbContent content
+	 * 返回值:E3mallResult
+	 */
+	public E3mallResult saveContent(TbContent content);
+	/**
+	 * 需求:查询首页加载广告数据
+	 * @param categoryId
+	 * @return
+	 */
+	public List<AdItem> findContentAdList(Long categoryId);
+}
